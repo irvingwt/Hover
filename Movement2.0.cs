70782e12453108cebed31ef62b30fa26eecb58bf
narvaezv <narvaezv94@gmail.com>
2015-09-24 12:33:41 -0500

    Update Movement2.0.cs

--- Movement2.0.cs
+++ Movement2.0.cs
@@ -3,33 +3,34 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
     public float speed;
-    private Vector2 vect;
-    private Vector2 myPosition;
+    private Vector3 vect;
+    private Vector3 myPosition;
     private float multiplier = 0;
     private bool click = true;
 
 	// Use this for initialization
 	void Start () {
-        
-	}
+        myPosition = transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        myPosition = transform.position;
-        if (Input.GetMouseButtonDown(0)) //Right click
-        {
-            vect = Camera.main.ScreenToWorldPoint(Input.mousePosition);//Gets position of click
-        }
         if (Input.GetMouseButtonUp(0) && click) //Release right click
         {
+            vect = Camera.main.ScreenToWorldPoint(Input.mousePosition);//Gets position of click
             GetComponent<Rigidbody2D>().AddForce((vect- myPosition).normalized * speed * multiplier);//Adds force
             click = false;
         }
-        if (Input.GetMouseButton(0)) //During right click
+        if (Input.GetMouseButton(0) && click) //During right click
         {
             multiplier += 2*(Time.deltaTime); //Adds multiplier
+            vect = Camera.main.ScreenToWorldPoint(Input.mousePosition);//Gets position of click
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, vect - transform.position);
         }
-        
+        if (transform.position.x < -300 || transform.position.x > 750)
+        {
+            Application.LoadLevel(1);
+        }
     }
 }
 
